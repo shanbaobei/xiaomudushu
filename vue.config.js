73f5160d1868f74bd9cccd3b7687033e76789b65
MININT-KDD4KI4\Administrator <1249556847@qq.com>
2020-08-11 16:19:22 +0800

    1、更改打包静态资源路径

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 
 module.exports = {
 
-  publicPath: '/',
+  publicPath: '.',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
